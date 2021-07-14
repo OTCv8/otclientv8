e2e56f82ed7760d98a5cf9908273aeccde38bc1e
OTCv8 <otclient@otclient.ovh>
2021-07-14 23:05:42 +0000

    Updated to OTCv8 3.0 rev 88

--- modules/gamelib/const.lua
+++ modules/gamelib/const.lua
@@ -205,6 +205,7 @@ GameMapDrawGroundFirst = 116 -- useful for big auras & wings
 GameMapIgnoreCorpseCorrection = 117
 GameDontCacheFiles = 118 -- doesn't work with encryption and compression
 GameBigAurasCenter = 119 -- Automatic negative offset for aura bigger than 32x32
+GameNewUpdateWalk = 120 -- Walk update rate dependant on FPS
 
 LastGameFeature = 130
         
